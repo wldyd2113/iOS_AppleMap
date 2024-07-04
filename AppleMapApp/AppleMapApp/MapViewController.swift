1a88560b34a507a94f640e2953a92387a2363ef2
jiyong <wldyd2113@gmail.com>
2024-07-04 15:48:08 +0900

    feat: 사용자 위치가져오기

--- AppleMapApp/AppleMapApp/MapViewController.swift
+++ AppleMapApp/AppleMapApp/MapViewController.swift
@@ -5,6 +5,8 @@ import CoreLocation
 class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     let locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    
     private var mapView: MKMapView!
     
     private lazy var currentButton: UIButton = {
@@ -18,6 +20,10 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager.requestWhenInUseAuthorization() //앱을 사용하는동안 사용자에게 위치서비스의 권한을 요청
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation() //사용자에게 현재위치를 알려줌
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
         mapView = MKMapView(frame: self.view.bounds)
         mapView.delegate = self
         self.view.addSubview(mapView)
@@ -32,16 +38,30 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
             currentButton.heightAnchor.constraint(equalToConstant: 50)
         ])
         
-
+        mapView.showsUserLocation = true
+        
     }
     
     //MARK: - 현재 위치를 반환해줌
     @objc func currentfetch() {
-        
+        if let location = currentLocation {
+            let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
+            mapView.setRegion(region, animated: true) //원하는 지역을 뜨워줌
+        }
     }
     
     //MARK: - CLLocationManagerDelegate
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-       
+        if let location = locations.first {
+            currentLocation = location
+            let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+        }
     }
 }
