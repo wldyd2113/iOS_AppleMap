f009ecad4c3fb72c3bed5cd925c21d049c31ebd4
jiyong <wldyd2113@gmail.com>
2024-07-04 14:46:40 +0900

    feat: button 작업중

--- AppleMapApp/AppleMapApp/MapViewController.swift
+++ AppleMapApp/AppleMapApp/MapViewController.swift
@@ -1,22 +1,47 @@
-//
-//  MapViewController.swift
-//  AppleMapApp
-//
-//  Created by 차지용 on 7/4/24.
-//
-
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
+    let locationManager = CLLocationManager()
     private var mapView: MKMapView!
-
+    
+    private lazy var currentButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "current"), for: .normal)
+        button.addTarget(self, action: #selector(currentfetch), for: .touchUpInside)
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mapView = MKMapView(frame: self.view.bounds)
+        mapView.delegate = self
         self.view.addSubview(mapView)
+        
+        self.view.addSubview(currentButton)
+        currentButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            currentButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            currentButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            currentButton.widthAnchor.constraint(equalToConstant: 50),
+            currentButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        
+
     }
     
+    //MARK: - 현재 위치를 반환해줌
+    @objc func currentfetch() {
+        
+    }
+    
+    //MARK: - CLLocationManagerDelegate
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+       
+    }
 }
